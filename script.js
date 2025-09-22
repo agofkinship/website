df290a00175762d8300ceed51e11356cf1012cce
Colin Yetman <colinguitarist@gmail.com>
2025-09-22 16:45:01 -0230

    add back to top button function - add dom content loader to avoid js crashes - add style sheet for guide pages - route all js back to main js - stop background from repeating, img is px, need higher res img

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
-const hamburger = document.querySelector(".hamburger");
-const navLinks = document.querySelector(".nav-links");
+// add dom content loaded event listener
+document.addEventListener("DOMContentLoaded", () => {
+	console.log("DOM fully loaded and parsed");
 
-hamburger.addEventListener("click", () => {
-  navLinks.classList.toggle("active");
+	// hamburger menu action
+
+	const hamburger = document.querySelector(".hamburger");
+	const navLinks = document.querySelector(".nav-links");
+
+	hamburger.addEventListener("click", () => {
+		navLinks.classList.toggle("active");
+	});
+
+	// back to top button
+
+	window.addEventListener("scroll", () => {
+		const backToTopButton = document.getElementById("back-to-top");
+		if (window.scrollY > 100) {
+			backToTopButton.style.display = "block";
+		} else {
+			backToTopButton.style.display = "none";
+		}
+
+		backToTopButton.addEventListener("click", () => {
+			window.scrollTo({
+				top: 0,
+				behavior: "smooth",
+			});
+		});
+	});
 });
